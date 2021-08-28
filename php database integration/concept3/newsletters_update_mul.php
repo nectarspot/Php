51b86e3222e00dd26f3f79a91013792611a56c28
NectarSpot <nectarspot@gmail.com>
2021-08-28 11:58:40 +0530

    Add files via upload

---
+++ php database integration/concept3/newsletters_update_mul.php
@@ -0,0 +1,30 @@
+<?php
+
+$DB_host = "localhost";
+	 $DB_user = "marketf7_lokesh";
+	 $DB_pass = "#Xka1ZJ$&QC[";
+	 $DB_name = "marketf7_mailspot";
+	 
+	 $MySQLiconn = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
+    
+     if($MySQLiconn->connect_errno)
+     {
+         die("ERROR : -> ".$MySQLiconn->connect_error);
+     }
+
+$id = $_POST['id'];
+
+$email = $_POST['email'];
+
+$date = $_POST['date'];
+$ip_address = $_POST['ip_address'];
+
+
+$chk = $_POST['chk'];
+$chkcount = count($id);
+for($i=0; $i<$chkcount; $i++)
+{
+	$MySQLiconn->query("UPDATE haywardlaw_newsletters SET email='$email[$i]', date='$date[$i]',  ip_address='$ip_address[$i]' WHERE id=".$id[$i]);
+}
+header("Location: https://marketenomicsdigital.com/haywardlaw-dashboard/newsletters-view.php");
+?>
